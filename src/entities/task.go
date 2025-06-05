33439a8348c2515e0cb5e4ea13ab2682cc90a905
lucalana <lucalana13@gmail.com>
2025-06-05 10:02:55 -0300

    feat: criando entidades

---
+++ src/entities/task.go
@@ -0,0 +1,15 @@
+package entities
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Task struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"ID"`
+	Title       string             `bson:"title" json:"title"`
+	Description string             `bson:"description" json:"description"`
+	CreatedAt   time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt   time.Time          `bson:"updatedAt" json:"updatedAt"`
+}
---
+++ src/entities/user.go
@@ -0,0 +1,15 @@
+package entities
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"ID"`
+	Email     string             `bson:"email" json:"email"`
+	Password  string             `bson:"password" json:"-"`
+	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
+}
