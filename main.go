243cae3cd9b60dd66ec3e7be3390856232b54647
lucalana <lucalana13@gmail.com>
2025-06-06 13:17:27 -0300

    feat: criando conexão com banco de dados

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"go-curso-udemy-extreme/src/repository"
+	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	uri := "mongodb://localhost:27017"
+	dbName := "curso-go"
+	repoUser, errUser := repository.NewUserRepository(uri, dbName, "users")
+	repoTask, errTask := repository.NewTaskRepository(uri, dbName, "tasks")
+
+	if errUser != nil || errTask != nil {
+		log.Fatalf("Erro ao iniciar repositório")
+		return
+	}
+
+	app := gin.Default()
+
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, Gin!"})
+	})
+
+	app.Run(":8000")
 }
